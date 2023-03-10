ccff04217e3407ac10517a71fd2b6e0640d99d03
PrateekKumar2109 <101380064+PrateekKumar2109@users.noreply.github.com>
2023-03-10 14:15:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ if st.button("Submit Your Query"):
 if st.session_state["generated"]:
 
     for i in range(len(st.session_state["generated"]) - 1, -1, -1):
-        message(st.session_state["past"][i], is_user=True,avatar_style="thumbs",seed='Felix',key=str(i) + "_user")
+        message(st.session_state["past"][i], is_user=True,avatar_style="thumbs",seed='Aneka',key=str(i) + "_user")
 
         message(st.session_state["generated"][i],avatar_style="fun-emoji", key=str(i))
         #message(st.session_state["generated"][i+1], key=str(i+1))
